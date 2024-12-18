4d9576345733ad1689f873723c5969f4b4027257
Jbm170 <juba.messaoudi@lgtlecorbusier.org>
2024-12-18 09:48:55 +0100

    client.h

--- client.h
+++ client.h
@@ -4,28 +4,38 @@
 #include <QObject>
 #include <QtNetwork/QTcpSocket>
 #include <QDebug>
-#define PORT_SERVEUR 8000
+#define PORT_SERVEUR 8000 // Définition du port du serveur
 
+// Déclaration de la classe Client
 class Client : public QObject
 {
     Q_OBJECT
 public:
+    // Constructeur explicite, avec un QObject parent par défaut
     explicit Client(QObject *parent = 0);
+    // Destructeur
     ~Client();
+
+    // Méthode pour démarrer la connexion au serveur
     void demarrer();
+
 private:
-    QTcpSocket *tcpSocket; // la socket
+    QTcpSocket *tcpSocket; // Pointeur vers un objet QTcpSocket pour gérer la connexion au serveur
 
 signals:
+    // Signaux (ajoutez ici si nécessaire)
 
 public slots:
+    // Slot appelé lorsque le client se connecte au serveur
     void estConnectee ();
+    // Slot appelé lorsque le client se déconnecte du serveur
     void estDeconnectee();
-    void gestionErreur(QAbstractSocket::SocketError );
+    // Slot appelé lorsqu'une erreur de socket se produit
+    void gestionErreur(QAbstractSocket::SocketError);
+    // Slot appelé lorsque des données sont reçues du serveur
     void recevoir();
-    void envoyer(QByteArray );
+    // Méthode pour envoyer des données au serveur
+    void envoyer(QByteArray);
 };
-
-
 
 #endif // CLIENT_H
